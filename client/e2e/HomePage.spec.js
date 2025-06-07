9ab0b46cefaa0d16ce8913104c4d9cc24d247d98
tarun-veeraraghavan-mv <tarun.veeraraghavan.dev@gmail.com>
2025-06-07 15:19:21 -0700

    e2e tests

---
+++ client/e2e/HomePage.spec.js
@@ -0,0 +1,31 @@
+import { test, expect } from "@playwright/test";
+
+test("Navbar should be rendered properly", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+
+  // Store elements in variables
+  const heading = page.getByRole("heading", { name: /ai code review/i });
+  const docsButton = page.getByRole("button", { name: /docs/i });
+  const reportsButton = page.getByRole("button", {
+    name: /reports/i,
+  });
+  const signinButton = page.getByRole("button", {
+    name: /sign in/i,
+  });
+
+  // Assert visibility
+  await expect(heading).toBeVisible();
+  await expect(docsButton).toBeVisible();
+  await expect(reportsButton).toBeVisible();
+  await expect(signinButton).toBeVisible();
+});
+
+test("Settings panel should work as expected", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+
+  const settingsButton = page.getByRole("button", {
+    name: /settings/i,
+  });
+
+  await expect(settingsButton).toBeVisible();
+});
