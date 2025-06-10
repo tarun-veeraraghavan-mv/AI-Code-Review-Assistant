3a43695fbd63b1ba67b06e3f93387c44b1e13820
tarun-veeraraghavan-mv <tarun.veeraraghavan.dev@gmail.com>
2025-06-10 16:03:35 -0700

    making sure backend runs and continues to next job2

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ const MONGO_URI =
   process.env.MONGO_URI ||
   "mongodb+srv://tarunv1911:ea0cj8dzRV2NhFmT@cluster0.70deyvr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
+console.log("Hello");
+
 console.log("KEY", process.env.OPENROUTER_API_KEY);
 
 connectDB(MONGO_URI).then(() => {
